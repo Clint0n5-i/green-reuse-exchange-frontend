0eed88e3477494169f94aa570a77b20d7ef5303c
clintons <clintonmg17@gmail.com>
2025-09-03 15:18:08 +0300

    notification

--- src/services/notificationApi.js
+++ src/services/notificationApi.js
@@ -1,11 +1,11 @@
 // Notification API service
-import axios from 'axios';
+import { api } from './api';
 
 export const fetchNotifications = async () => {
-  const res = await axios.get('/api/notifications');
+  const res = await api.get('/notifications');
   return res.data;
 };
 
 export const markNotificationRead = async (id) => {
-  await axios.post(`/api/notifications/${id}/read`);
+  await api.post(`/notifications/${id}/read`);
 };
